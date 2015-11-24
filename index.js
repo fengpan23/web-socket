47aeb3da5b94f5f27a6b5d220818bb904bb5a66a
fengpan <pan.feng@stargt.com.cn>
2015-11-24 11:38:18 +0800

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.use(function(req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+  	next();
+});
+app.use(express.static(__dirname + '/public'));
+
+
+
+// app.get('/', function(req, res){
+//   	res.send('hello world');
+// });
+
+app.listen(3000);
